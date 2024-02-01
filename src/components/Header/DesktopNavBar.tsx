171f69b282c20b1e91f66801e7064dbf524653b0
Ian Frye <ian_frye@outlook.com>
2024-02-01 06:55:07 -0500

    added singout and dashboard buttons for logged in users

--- src/components/Header/DesktopNavBar.tsx
+++ src/components/Header/DesktopNavBar.tsx
@@ -8,6 +8,67 @@ const navLinks = [
 ];
 
 export default function DesktopNavBar() {
+  const user = true;
+
+  const authLinks = user ? (
+    <>
+      <NavLink
+        style={{
+          marginLeft: '10px',
+          color: 'white',
+          backgroundColor: '#ff8c38',
+          padding: '10px',
+          borderRadius: '5px',
+          textDecoration: 'none',
+        }}
+        path={'/dashboard'}
+        text={'Dashboard'}
+      />
+      <button
+        style={{
+          color: 'white',
+          backgroundColor: '#1f1f1f',
+          padding: '11px',
+          borderRadius: '5px',
+          marginLeft: '10px',
+          textDecoration: 'none',
+          border: 'none',
+          cursor: 'pointer',
+          textTransform: 'uppercase',
+        }}
+        onClick={() => console.log('sign out')}
+      >
+        Sign Out
+      </button>
+    </>
+  ) : (
+    <>
+      <NavLink
+        style={{
+          color: 'white',
+          backgroundColor: '#ff8c38',
+          padding: '10px',
+          borderRadius: '5px',
+          marginLeft: '10px',
+          textDecoration: 'none',
+        }}
+        path={'/sign-in'}
+        text={'Sign In'}
+      />
+      <NavLink
+        style={{
+          color: 'white',
+          backgroundColor: '#6c757d',
+          padding: '10px',
+          borderRadius: '5px',
+          marginLeft: '10px',
+          textDecoration: 'none',
+        }}
+        path={'/sign-up'}
+        text={'Sign Up'}
+      />
+    </>
+  );
   return (
     <nav className='desktop-nav-bar'>
       <div className='logo'>
@@ -22,20 +83,7 @@ export default function DesktopNavBar() {
           />
         ))}
       </ul>
-      <div className='auth-links'>
-        <NavLink
-          style={{
-            color: 'white',
-            backgroundColor: '#ff8c38',
-            padding: '10px',
-            borderRadius: '5px',
-            marginLeft: '10px',
-            textDecoration: 'none',
-          }}
-          path={'/sign-in'}
-          text={'Sign In'}
-        />
-      </div>
+      <div className='auth-links'>{authLinks}</div>
     </nav>
   );
 }
