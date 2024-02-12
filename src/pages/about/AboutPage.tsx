cd87399f159de5eb85f9ede7c65017aff3b9bd8f
Ian <ian_frye@outlook.com>
2024-02-12 10:45:31 -0500

    fixed spacing on homepage/aboutpage

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -13,7 +13,7 @@ export default function About() {
             alt='Hero Img'
           />
         </div>
-        <div className='flex flex-col justify-between gap-12 p-4 md:px-24 mt-12'>
+        <div className='flex flex-col max-w-[1280px] mx-auto justify-between gap-12 p-4 md:px-24 mt-12'>
           <h1 className='text-3xl md:text-6xl font-bold'>
             Don't squeeze in a sedan when you could relax in a van.
           </h1>
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -8,19 +8,21 @@ export default function Home() {
   return (
     <Layout>
       <main className='h-screen relative z-2 md:p-28 flex flex-col items-center gap-8 justify-evenly p-4 md:justify-center text-white homepage '>
-        <h1 className='text-4xl md:text-7xl font-bold '>
-          You got the travel plans, we got the travel vans.
-        </h1>
-        <p className='text-[#c8c7c7] text-3xl'>
-          Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make
-          your perfect road trip.
-        </p>
-        <Link
-          className='w-full p-3 bg-orange-500 text-white text-center text-2xl md:text-3xl font-bold hover:bg-orange-600 transition-all duration-300 ease-in-out'
-          to={'/vans'}
-        >
-          Find your van
-        </Link>
+        <div className='h-full max-w-[1280px] flex flex-col justify-evenly md:justify-center gap-12'>
+          <h1 className='text-4xl md:text-7xl font-bold '>
+            You got the travel plans, we got the travel vans.
+          </h1>
+          <p className='text-[#c8c7c7] text-3xl'>
+            Add adventure to your life by joining the #vanlife movement. Rent the perfect van to
+            make your perfect road trip.
+          </p>
+          <Link
+            className='w-full p-3 bg-orange-500 text-white text-center text-2xl md:text-3xl font-bold hover:bg-orange-600 transition-all duration-300 ease-in-out'
+            to={'/vans'}
+          >
+            Find your van
+          </Link>
+        </div>
       </main>
     </Layout>
   );
