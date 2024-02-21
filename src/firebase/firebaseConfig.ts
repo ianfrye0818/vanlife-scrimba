cfaf65c5664f2bfdcfc22226ab585d42cd47c7a1
Ian Frye <ian_frye@outlook.com>
2024-02-21 05:09:45 -0500

    moving firebase variables to .env.llocal

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
-  apiKey: 'AIzaSyBDpzIoKjWsUSTikt-fOx-AmtwhJl0jZUc',
-  authDomain: 'vanlife3-7cc12.firebaseapp.com',
-  projectId: 'vanlife3-7cc12',
-  storageBucket: 'vanlife3-7cc12.appspot.com',
-  messagingSenderId: '48542450040',
-  appId: '1:48542450040:web:6b2cbdaf222dd7bc51c88d',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
