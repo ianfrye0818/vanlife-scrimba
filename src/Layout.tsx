dbaa387baed67ea3e62f5c2f1b3c2ca5fa3455f7
Ian Frye <ian_frye@outlook.com>
2024-03-02 19:16:04 -0500

    fixed styling issue min height

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -8,7 +8,7 @@ import Footer from './components/Footer/Footer';
 //default layout for entire file - adds navbar and footer to all pages
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <div className='h-screen'>
+    <div className='m-h-screen'>
       <Navbar />
       {children}
       <Footer />
